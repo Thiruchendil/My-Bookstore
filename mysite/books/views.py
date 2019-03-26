7f415bd0beed94ab444bcad13c72dcecb15e2d42
Thiruchendil <saha_thiru@yahoo.com>
2019-03-27 00:43:30 +0530

    Added login forms

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from . models import Book
-from django.http import Http404
+from django.http import HttpResponse
 from django.views import generic
 from django.views.generic.edit import CreateView
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home(request):
+  return HttpResponse('Home Page')
 #Views in normal format
 """def index(request):
     all_books = Book.objects.all()
@@ -10,7 +15,6 @@ from django.views.generic.edit import CreateView
         'all_books':all_books
     }
     return render(request,'books/index.html',context)
-
 def detail(request,book_id):
     try:
         book = Book.objects.get(id=book_id)
@@ -34,4 +38,4 @@ class bookcreate(CreateView):
 
 class Detailview(generic.DetailView):
     model = Book
-    template_name = 'books/detail.html'
+template_name = 'books/detail.html'
